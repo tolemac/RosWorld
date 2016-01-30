d34bdcd7df7bf85d1157e815a9beecf63007e8f2
Javier Ros (tolemaC) <tolemac@jros.org>
2016-01-30 03:40:54 +0100

    game controller

---
+++ src/RosWorldWeb/typescript/app/game-controller.ts
@@ -0,0 +1,56 @@
+﻿/// <reference path="../typings/tsd.d.ts" />
+
+class GameController {
+
+    static $inject = ["$http", "$interval"];
+    constructor(private $http: ng.IHttpService, $interval : ng.IIntervalService) {
+        //this.updateEngine();
+
+        $interval(() => {
+            this.updateEngine();
+            this.updateEntities();
+        }, 500);
+    }
+
+    engine: string;
+    entities: string;
+
+    gameMessage: string;
+
+    updateEngine() {
+        this.$http.post("/Game/GetEngine", {}).then((response: any) => {
+            this.engine = response.data;
+        });
+    }
+
+    updateEntities() {
+        this.$http.post("/Game/GetEntities", {}).then((response: any) => {
+            this.entities = JSON.stringify(response.data, null, "\t");
+        });
+    }
+
+    clearMessage() {
+        this.setMessage("");
+    }
+
+    setMessage(msg) {
+        this.gameMessage = msg;
+    }
+
+    player1BuildHouse() {
+        this.clearMessage();
+        this.$http.post("/Game/Player1BuildHouse", {}).then((response: any) => {
+            if (response.data !== "True")
+                this.setMessage("No tiene oro suficiente para construir casas, cada casa vale 100 de oro y crea 5 personas para coger más oro.");
+
+        });
+    }
+
+    reset() {
+        this.clearMessage();
+        this.$http.post("/Game/Reset", {});
+    }
+
+}
+
+angular.module("app").controller("gameController", GameController);
